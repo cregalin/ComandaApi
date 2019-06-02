42f0a68cd62804c106eadea7dd991cce72713b53
Caroline <33700047+cregalin@users.noreply.github.com>
2019-06-02 17:40:09 -0300

    Update comanda.rb

--- RestAPIFurb/app/models/comanda.rb
+++ RestAPIFurb/app/models/comanda.rb
@@ -1,5 +1,6 @@
 class Comanda < ApplicationRecord
     validates :produtos, length: { maximum: 200 }, presence: true
-    validates_numericality_of :valortotal, precision: 10, scale: 2, greater_than_or_equal_to: 0
+    #precision: 10, scale: 2, - verificar essa validaçâo
+    validates_numericality_of :valortotal, greater_than_or_equal_to: 0
     belongs_to :usuario
   end
