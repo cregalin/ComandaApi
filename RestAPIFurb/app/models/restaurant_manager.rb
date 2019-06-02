2082592065a0e3c38b4161db716028a4e5ccbed5
Caroline <carol_regalin@hotmail.com>
2019-06-02 01:02:46 -0300

    Validações do model

--- RestAPIFurb/app/models/restaurant_manager.rb
+++ RestAPIFurb/app/models/restaurant_manager.rb
@@ -1,2 +1,5 @@
 class RestaurantManager < ApplicationRecord
-end
+    validates :products, maximum: 200, presence: true
+    validates_numericality_of :products_amount, precision: 10, scale: 2, greater_than_or_equal_to: 0
+    belongs_to :user
+  end
--- RestAPIFurb/app/models/user.rb
+++ RestAPIFurb/app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    has_many :restaurant_manager
+    validates :title, maximum: 50, presence: true
+    validates :password, maximum: 100, presence: true
 end
