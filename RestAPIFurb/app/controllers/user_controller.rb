16163d69cedf8fff38558f9ed3af4020a4326962
Caroline <carol_regalin@hotmail.com>
2019-06-02 01:23:54 -0300

    Ajuste validações + rotas + ajuste controller

--- RestAPIFurb/app/controllers/user_controller.rb
+++
@@ -1,6 +0,0 @@
-class UserController < ApplicationController   
-    def index
-        users = User.all;
-        render json: {status: 'SUCCESS', data: users},status: :ok
-    end
-end
--- RestAPIFurb/app/models/user.rb
+++ RestAPIFurb/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     has_many :restaurant_manager
-    validates :title, maximum: 50, presence: true
-    validates :password, maximum: 100, presence: true
+    validates :title, length: { maximum: 50 }, presence: true
+    validates :password, length: { maximum: 100 }, presence: true
 end
