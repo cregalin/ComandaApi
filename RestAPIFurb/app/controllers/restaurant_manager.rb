62014c2da4993048c56d36875e6a70ae5269e3b1
Caroline <carol_regalin@hotmail.com>
2019-06-02 01:51:23 -0300

    Actions controllers

--- RestAPIFurb/app/controllers/restaurant_manager.rb
+++ RestAPIFurb/app/controllers/restaurant_manager.rb
@@ -1,2 +1,23 @@
-class RestaurantManagerController < ApplicationController   
+class RestaurantManagerController < ApplicationController  
+    def index
+    end
+
+	def show
+    end
+
+    def create
+    end
+
+    #PUT
+    def update
+    end
+
+    def destroy
+    end
+
+    
+    #DELETE
+    #DELETE by ID
+    #verificar AI comanda banco de dados
+    #validar parametros enviados
 end
--- RestAPIFurb/app/controllers/users_controller.rb
+++ RestAPIFurb/app/controllers/users_controller.rb
@@ -1,6 +1,41 @@
 class UsersController < ApplicationController   
+    #Listar todos os  usuários
     def index
         users = User.all
         render json: users, status: :ok
     end
+
+    # Listar usuários passando ID 
+	def show
+        user = User.find(params[:id])
+        render json: user, status: :ok
+        #verificar tratamento quando o usuário não é encontrado
+    end
+
+    #Cria um novo usuário POST
+    def create
+        #to do test
+        debugger
+        user = User.new(params["user"])
+        debugger
+        if user.save
+            render json: user, status: :ok
+        else
+            render json: user.erros, status: :unprocessable_entity
+        end
+    end
+
+    #PUT
+    def update
+    end
+
+    def destroy
+        #to do
+    end
+
+    
+    #DELETE
+    #DELETE by ID
+    #verificar AI no id do usuario e comanda banco de dados
+    #validar parametros enviados
 end
